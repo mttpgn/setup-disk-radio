d71b643af7ac68173873326f661d7865a138fb4b
Matthew Pagan <matthew.a.pagan@gmail.com>
2017-05-11 02:18:32 +0000

    Add more to the setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,12 @@
 
 # Make mp3 directory
 mkdir /srv/mp3
+chmod 777 /srv/mp3 # We'll be regularly writing to this dir.
 
 # Copy scripts to /opt
 REPO="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -a $REPO /opt
+cp -a $REPO /opt/
+chmod +x /opt/setup-disk-radio/*sh
+
+# First run to get some songs
+python2 /opt/setup-disk-radio/musicscope.py
