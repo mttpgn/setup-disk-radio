3da0b0c773e5fc5a030982dcf9eda714c59f91c4
Matthew Pagan <matthew.a.pagan@gmail.com>
2017-05-11 01:26:00 +0000

    Set the mp3 directory as a variable.

--- clearoldsongs.sh
+++ clearoldsongs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+DIR='/srv/mp3'
 ADEQUATESPACE=90
-PERCENTFREE=`df -k /srv/mp3 | awk '{if ($1 != "Filesystem") print $5 " "}'`
+PERCENTFREE=`df -k $DIR | awk '{if ($1 != "Filesystem") print $5 " "}'`
 SPACEFREE=`echo $PERCENTFREE | tr -d '%'`
-echo $PERCENTFREE $SPACEFREE space currently used on disk.
+echo $SPACEFREE percent of space currently used on disk.
 
 CTR=0
 while  [[ "$SPACEFREE" -gt "$ADEQUATESPACE" ]]; do
-    F=`find /srv/mp3 -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d \  -f 2`
+    F=`find $DIR -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d \  -f 2`
     rm -fv $F
-    SPACEFREE=`df -k /srv/mp3 | egrep "([90][0-9]|100)%"`
+    SPACEFREE=`df -k $DIR | egrep "([90][0-9]|100)%"`
     CTR=$((CTR+1))
 done
 
